41aa4df31f5e16eb1c5a960f7ef3274e6f472550
Ankit Kumar <kumar.ankit383@gmail.com>
2020-10-15 08:49:42 +0530

    Odd one out

---
+++ Data Structures/Merge.py
@@ -0,0 +1,29 @@
+#   Merge two arrays withouy any extra space i.e. in O(1) space complexity
+import math
+
+#   naive approach
+def merge(arr : list, brr : list) -> list : 
+
+    i = 0; j = 0 
+
+    while i <= len(arr) and j <= len(brr) : 
+        pass
+    else : 
+        pass
+
+
+#   optimised approach using the GAP Algorithm
+
+def optimisedmerge(arr, arr2) : 
+    
+    gap = math.ceil((len(arr) + len(arr2)) / 2)
+
+    while gap != 0 : 
+
+        
+                
+        gap //= 2
+
+    
+            
+            
