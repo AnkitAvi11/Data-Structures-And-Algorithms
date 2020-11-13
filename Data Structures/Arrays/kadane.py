2e218b2556d3721c8b79ee46dc6917940cf125db
unknown <kumar.ankit383@gmail.com>
2020-11-13 20:38:07 +0530

    Kadane's algorithm

---
+++ Data Structures/Arrays/kadane.py
@@ -0,0 +1,20 @@
+#   kadanes' algorithms
+
+def max_contiguos_sum(arr : list) : 
+    sum_so_far = 0;current_sum = 0
+
+    for el in arr : 
+        current_sum+=el
+
+        if current_sum > sum_so_far : 
+            sum_so_far = current_sum
+
+        if current_sum < 0 : 
+            current_sum = 0 
+
+    return sum_so_far
+    
+
+if __name__ == "__main__":
+    print(max_contiguos_sum([-2,-3,4,-1,-2,1,5,-3]))
+    
