fc99b5d2c5183ec42b0fcba35024376dda50d9c2
unknown <kumar.ankit383@gmail.com>
2020-11-16 15:51:01 +0530

    Generate all possible sub string of a given string

---
+++ Problems/generate.py
@@ -0,0 +1,26 @@
+"""
+
+Generate all the strings of length n drawn from 0 ... k -1
+
+"""
+
+
+def get_substring(string, counter, result, level, k) : 
+    if level == k : 
+        print(result)
+
+    else : 
+
+        for i in range(len(string)) : 
+            if counter[i] == 1 : 
+                result[level] = string[i]
+                counter[i] = 0
+                get_substring(string, counter, result, level+1, k)
+                counter[i] = 1
+
+
+if __name__ == '__main__' : 
+    string = "abcdefgh"
+    counter = [1 for _ in range(len(string))] 
+    result = [0 for _ in range(len(string))]
+    get_substring(string, counter, result,0, 4)
--- Problems/permutation.py
+++ Problems/permutation.py
@@ -1,21 +1,22 @@
 #   string permutation
 
-def string_permutation(str, counter, result, level) : 
-    if level == len(str) : 
+def string_permutation(string, counter, result, level) : 
+    if level == len(string) : 
         print(result)
 
     else : 
-        for i in range(len(str)) : 
-            if counter[i] == 1 : 
-                result[level] = str[i]
-                counter[i] = 0
-                string_permutation(str, counter, result, level+1)
-                counter[i] = 1
-            else : 
+
+        for i in range(len(string)) : 
+            if counter[i] == 0 : 
                 continue
+            else : 
+                result[level] = string[i]
+                counter[i] = 0
+                string_permutation(string, counter, result, level+1)
+                counter[i] = 1
 
 
 if __name__ == "__main__":
     counter = [1,1,1]
     result = [0,0,0]
-    string_permutation(['a', 'b', 'c'], counter, result, 0)
+    string_permutation(['1', '2', '3'], counter, result, 0)
