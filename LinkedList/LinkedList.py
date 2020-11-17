38f2ba46c6cb6dd02774f1f539dca17008cdf264
unknown <kumar.ankit383@gmail.com>
2020-11-17 21:08:13 +0530

    Some matrix problems : easy

---
+++ LinkedList/LinkedList.py
@@ -0,0 +1,22 @@
+
+class ListNode : 
+    
+    def __init__(self) : 
+        self.data = None
+        self.next_node = None 
+
+
+#   for the linked list
+class LinkedList : 
+
+    def __init__(self) : 
+        self.head = None
+
+
+    def append_to_list(self, sk) : 
+        ptr = ListNode()
+        
+    
+
+if __name__ == "__main__":
+    pass
