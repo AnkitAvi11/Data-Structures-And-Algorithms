4ca1be2cc84cedbb3ecdadfcc2269865001d6dfd
unknown <kumar.ankit383@gmail.com>
2020-12-15 20:25:04 +0530

    Merge intervals

--- Arrays/Meeting.py
+++ Arrays/Meeting.py
@@ -3,17 +3,22 @@ Your company built an in-house calendar tool called hiCal. You want to add a fea
 """
 
 def hiCal(arr : list) -> list : 
-    result = list()
+    #   sorting by the order of start time
+    arr.sort()
 
-    for i in range(len(arr)-1) : 
-        for j in range(i+1, len(arr)) : 
-            
-            if arr[j][0] <= arr[i][0] <= arr[j][1] or arr[j][0] <= arr[i][1] <= arr[j][1] or arr[i][0] <= arr[j][0] <= arr[i][1] or arr[i][0] <= arr[j][1] <= arr[i][1] : 
-                print('came here')
-                result.append((min(arr[i][0], arr[j][0]), max(arr[i][1], arr[j][1])))
+    merged_meetinds = [arr[0]]
 
-    return result
+    for current_start_time, current_end_time in arr[1:] : 
+        last_start_time, last_end_time = merged_meetinds[-1]
 
+        if current_start_time <= last_end_time :
+            merged_meetinds[-1] = (last_start_time, max(current_start_time, last_end_time))
+
+        else : 
+            merged_meetinds.append((current_start_time, current_end_time))
+    
+
+    return merged_meetinds
 
 if __name__ == '__main__' : 
     print(hiCal([(5,8),(1,4),(6,8)]))
